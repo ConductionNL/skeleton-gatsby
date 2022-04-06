dd107532465bdecccb05fc14fcfa370d6df5a9e5
Barry Brands <barrybrands02@hotmail.com>
2022-04-06 14:43:35 +0200

    cases test page added

---
+++ pwa/src/pages/cases/index.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+// Disabled till fixed
+// import { Article, Heading1 } from "@nl-design-system-unstable/example-next.js/src/components/utrecht";
+
+// To make Utrechts components working for now, copy a component to our /pwa/src/components/utrecht-components
+// directory and uncomment the export in utrecht-components/index.ts
+import { Heading1 } from "../../components/utrecht-components";
+
+const IndexPage = () => {
+  return (
+    <>
+      <Heading1>Zaken</Heading1>
+    </>
+  );
+};
+
+export default IndexPage;
