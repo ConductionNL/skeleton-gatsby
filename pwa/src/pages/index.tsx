09834c9c69fdaecdaa8536992e3dc80bcd21d1d6
Barry Brands <barrybrands02@hotmail.com>
2022-04-06 09:48:56 +0200

    index page Heading1 component used

--- pwa/src/pages/index.tsx
+++ pwa/src/pages/index.tsx
@@ -1,9 +1,16 @@
 import * as React from "react";
 
+// Disabled till fixed
+// import { Article, Heading1 } from "@nl-design-system-unstable/example-next.js/src/components/utrecht";
+
+// To make Utrechts components working for now, copy a component to our /pwa/src/components/utrecht-components
+// directory and uncomment the export in utrecht-components/index.ts
+import { Heading1 } from "../components/utrecht-components";
+
 const IndexPage = () => {
   return (
     <>
-      <h1>Welcome to the skeleton app</h1>
+      <Heading1>Welcome to the skeleton</Heading1>
     </>
   );
 };
