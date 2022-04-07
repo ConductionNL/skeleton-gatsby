4e4558a93d87d504dcab6b1092fb18b174613c06
Barry Brands <barrybrands02@hotmail.com>
2022-04-07 11:14:12 +0200

    Index page cleanup

--- pwa/src/pages/index.tsx
+++ pwa/src/pages/index.tsx
@@ -1,21 +1,12 @@
 import * as React from "react";
-import { isLoggedIn, logout } from "../services/auth";
+import { isLoggedIn } from "../services/auth";
+import { Heading1, Article } from "../components/utrecht-components";
 
-// Disabled till fixed
-// import { Article, Heading1 } from "@nl-design-system-unstable/example-next.js/src/components/utrecht";
-
-// To make Utrechts components working for now, copy a component to our /pwa/src/components/utrecht-components
-// directory and uncomment the export in utrecht-components/index.ts
-import { Heading1 } from "../components/utrecht-components";
-
-const IndexPage = () => {
+const IndexPage: React.FC = () => {
   return (
     <>
-      <Heading1>Welcome to the skeleton</Heading1>
-
-      <p>U are logged in</p>
-
-      <button onClick={logout}>Logout</button>
+      <Heading1>Skeleton Application</Heading1>
+      {isLoggedIn() && <Article>U are logged in</Article>}
     </>
   );
 };
