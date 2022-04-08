6d88fa72896b72f2b9b354e2a585928145d85840
Sarai Misidjan <Sarai@conduction.nl>
2022-04-08 13:46:31 +0200

    added font awesome icons to footer

--- pwa/src/components/footer/Footer.tsx
+++ pwa/src/components/footer/Footer.tsx
@@ -1,37 +1,39 @@
 import React from "react";
 import {
-    PageFooter,
-    UnorderedList,
-    UnorderedListItem,
-    Heading4
+  Heading4,
+  PageFooter,
+  UnorderedList,
+  UnorderedListItem
 } from "@nl-design-system-unstable/example-next.js/src/components/utrecht";
 import "./Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone, faBuilding, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Footer: React.FC = () => {
-    return (
-        <PageFooter>
-            <UnorderedList>
-                <Heading4>Diensten</Heading4>
-                <UnorderedListItem>Verhuizen</UnorderedListItem>
-                <UnorderedListItem>Huwelijk</UnorderedListItem>
-                <UnorderedListItem>Uittreksel</UnorderedListItem>
-            </UnorderedList>
+  return (
+    <PageFooter>
+      <UnorderedList>
+        <Heading4>Diensten</Heading4>
+        <UnorderedListItem>Verhuizen</UnorderedListItem>
+        <UnorderedListItem>Huwelijk</UnorderedListItem>
+        <UnorderedListItem>Uittreksel</UnorderedListItem>
+      </UnorderedList>
 
-            <UnorderedList>
-                <Heading4>Contact</Heading4>
-                <UnorderedListItem>Stadswinkel, Marienburg 30</UnorderedListItem>
-                <UnorderedListItem><utrecht-icon-whatsapp/>0612345678</UnorderedListItem>
-                <UnorderedListItem>gemeente@utrecht.nl</UnorderedListItem>
-            </UnorderedList>
+      <UnorderedList>
+        <Heading4>Contact</Heading4>
+        <UnorderedListItem><FontAwesomeIcon icon={faBuilding} />Stadswinkel, Marienburg 30</UnorderedListItem>
+        <UnorderedListItem><FontAwesomeIcon icon={faPhone} />0612345678</UnorderedListItem>
+        <UnorderedListItem><FontAwesomeIcon icon={faEnvelope} />gemeente@utrecht.nl</UnorderedListItem>
+      </UnorderedList>
 
-            <UnorderedList>
-                <Heading4>Openingstijden</Heading4>
-                <UnorderedListItem>Maandag - woensdag: 9.00 - 17.00</UnorderedListItem>
-                <UnorderedListItem>Donderdag: 9.00 - 20.00</UnorderedListItem>
-                <UnorderedListItem>Vrijdag: 9.00 - 17.00</UnorderedListItem>
-            </UnorderedList>
-        </PageFooter>
-    );
+      <UnorderedList>
+        <Heading4>Openingstijden</Heading4>
+        <UnorderedListItem>Maandag - woensdag: 9.00 - 17.00</UnorderedListItem>
+        <UnorderedListItem>Donderdag: 9.00 - 20.00</UnorderedListItem>
+        <UnorderedListItem>Vrijdag: 9.00 - 17.00</UnorderedListItem>
+      </UnorderedList>
+    </PageFooter>
+  );
 };
 
 export default Footer;
