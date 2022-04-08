bd8cbae57b8cc2f202e0cec983ff7bc90f2f99ec
Lennart <66552932+lencodes@users.noreply.github.com>
2022-04-08 16:00:07 +0200

    REFACTOR LoginForm to react-hook-form and new file

---
+++ pwa/src/forms/LoginForm.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import { useForm } from "react-hook-form";
+import { navigate } from "gatsby-link";
+import { Button } from "@nl-design-system-unstable/example-next.js/src/components/utrecht";
+import { setUser } from "./../services/auth";
+
+import APIService from "../apiService/apiService";
+import { InputText, InputPassword } from "../components/formFields";
+
+export const LoginForm: React.FC = () => {
+  const API: APIService = new APIService("");
+  const [loading, setLoading] = React.useState<boolean>(false);
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = (data: any) => {
+    setLoading(true);
+
+    API.Login.login(data)
+      .then((res) => {
+        const user = { username: res.data.username };
+
+        setUser(user);
+        sessionStorage.setItem("jwt", res.data.jwtToken);
+        sessionStorage.setItem("user", JSON.stringify(user));
+
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(`Login went wrong: ${err}`);
+        throw new Error(`Login went wrong: ${err}`);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <InputText name="username" label="Gebruikersnaam" {...{ errors, register }} validation={{ required: true }} />
+      <InputPassword name="password" label="Wachtwoord" {...{ errors, register }} validation={{ required: true }} />
+
+      <Button type="submit" disabled={loading}>
+        Verzenden
+      </Button>
+    </form>
+  );
+};
--- pwa/src/pages/login/index.tsx
+++ pwa/src/pages/login/index.tsx
@@ -1,45 +1,10 @@
 import * as React from "react";
-import APIService from "../../apiService/apiService";
-import { setUser, isLoggedIn } from "../../services/auth";
+import { isLoggedIn } from "../../services/auth";
 import { navigate } from "gatsby-link";
-import {
-  Heading1,
-  FormField,
-  Textbox,
-  Button,
-} from "@nl-design-system-unstable/example-next.js/src/components/utrecht";
+import { Heading1 } from "@nl-design-system-unstable/example-next.js/src/components/utrecht";
+import { LoginForm } from "../../forms/LoginForm";
 
 const Login: React.FC = () => {
-  const [loading, setLoading] = React.useState<boolean>(false);
-  const API: APIService = new APIService("");
-  const login = (e) => {
-    e.preventDefault();
-
-    setLoading(true);
-    let body = {
-      username: e.target.username?.value,
-      password: e.target.password?.value,
-    };
-
-    API.Login.login(body)
-      .then((res) => {
-        const user = { username: res.data.username };
-
-        setUser(user);
-        sessionStorage.setItem("jwt", res.data.jwtToken);
-        sessionStorage.setItem("user", JSON.stringify(user));
-
-        navigate("/");
-      })
-      .catch((err) => {
-        console.log(`Login went wrong: ${err}`);
-        throw new Error(`Login went wrong: ${err}`);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
-
   React.useEffect(() => {
     if (isLoggedIn() && window.location.pathname.includes("login")) {
       navigate("/");
@@ -50,16 +15,7 @@ const Login: React.FC = () => {
     <>
       <Heading1>Welcome to the Skeleton!</Heading1>
 
-      <form onSubmit={login}>
-        <FormField>
-          <Textbox name="username" type="text" required />
-        </FormField>
-        <FormField>
-          <Textbox name="password" type="password" required />
-        </FormField>
-
-        <Button type="submit">{!loading ? "Login" : "Loading..."}</Button>
-      </form>
+      <LoginForm />
     </>
   );
 };
