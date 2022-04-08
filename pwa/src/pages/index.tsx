819e5593a6a03554777a26e4f9e1fa246c6f70e0
Barry Brands <barrybrands02@hotmail.com>
2022-04-08 15:42:49 +0200

    import index.css to layout

--- pwa/src/pages/index.tsx
+++ pwa/src/pages/index.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import "./../styling/index.css";
 import { isLoggedIn } from "../services/auth";
 import { Heading1, Article } from "@nl-design-system-unstable/example-next.js/src/components/utrecht";
 
