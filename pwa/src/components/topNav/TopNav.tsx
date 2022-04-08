647791cf02115624bbf3c87a39cddec6c1faafd2
Remko <remko@conduction.nl>
2022-04-08 12:20:23 +0200

    added part of the topnav

---
+++ pwa/src/components/topNav/TopNav.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import "./TopNav.css";
+
+export const TopNav: React.FC = () => (
+  <div className="TopNav">
+    <div>Logo</div>
+    <div>Links</div>
+  </div>
+);
--- pwa/src/layout/Layout.tsx
+++ pwa/src/layout/Layout.tsx
@@ -1,11 +1,12 @@
 import { Document, Page, PageContent } from "@nl-design-system-unstable/example-next.js/src/components/utrecht";
+import { TopNav } from "../components/topNav/TopNav";
 
 const Layout: React.FC = ({ children }) => {
   return (
     <>
       <title>Skeleton Application</title>
-
       <Document>
+        <TopNav />
         <Page>
           <PageContent>{children}</PageContent>
         </Page>
