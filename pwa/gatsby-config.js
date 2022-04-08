0ca166fa5da2758adf26f7ecfc635b0076975239
Remko <remko@conduction.nl>
2022-04-08 14:47:54 +0200

    added topnav

--- pwa/gatsby-config.js
+++ pwa/gatsby-config.js
@@ -10,5 +10,13 @@ module.exports = {
         component: require.resolve(`./src/layout/Layout.tsx`),
       },
     },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
   ],
 };
--- pwa/src/components/topNav/TopNav.tsx
+++ pwa/src/components/topNav/TopNav.tsx
@@ -1,13 +1,18 @@
 import * as React from "react";
 import { Container } from "../container/Container";
 import "./TopNav.css";
+import Logo from "./../../assets/logo.svg";
 
 export const TopNav: React.FC = () => (
   <div className="TopNav">
     <Container>
       <div className="TopNav-inner">
-        <div>Logo</div>
-        <div>Links</div>
+        <Logo />
+
+        <ul className="TopNav-userNav">
+          <li>John Doe</li>
+          <li>Uitloggen</li>
+        </ul>
       </div>
     </Container>
   </div>
