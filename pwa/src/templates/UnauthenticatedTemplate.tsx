ba3f8978c4d9bf496ee6452273a44e4fc64b7e3a
Barry Brands <barrybrands02@hotmail.com>
2022-04-12 17:07:31 +0200

    getProducts on index page

--- pwa/src/templates/UnauthenticatedTemplate.tsx
+++ pwa/src/templates/UnauthenticatedTemplate.tsx
@@ -1,8 +1,29 @@
 import * as React from "react";
 import { Button, Heading1 } from "@nl-design-system-unstable/example-next.js/src/components/utrecht";
 import { Link } from "gatsby";
+import { ProductGrid } from "../components/products/ProductGrid/ProductGrid";
+import APIService from "../apiService/apiService";
+import APIContext from "../apiService/apiContext";
 
 export const UnauthenticatedTemplate: React.FC = () => {
+  const [products, setProducts] = React.useState(null);
+  const API: APIService | null = React.useContext(APIContext);
+
+  React.useEffect(() => {
+    !products && getProducts();
+  }, [API]);
+
+  const getProducts = () => {
+    API &&
+      API.APICalls.getAll("products")
+        .then((res) => {
+          res.data.total && setProducts(res.data.results);
+        })
+        .catch((err) => {
+          throw new Error(err);
+        });
+  };
+
   return (
     <>
       <Heading1>Welcome to the Skeleton Application</Heading1>
@@ -10,6 +31,8 @@ export const UnauthenticatedTemplate: React.FC = () => {
       <Link to="/login">
         <Button>Login</Button>
       </Link>
+
+      <ProductGrid products={products} />
     </>
   );
 };
