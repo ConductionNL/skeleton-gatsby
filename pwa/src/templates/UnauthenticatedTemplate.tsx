a73f39140b755c045da7d18661039c18c4e50739
Barry Brands <barrybrands02@hotmail.com>
2022-04-13 09:14:50 +0200

    Remove duplicated Document, Page and PageContent

--- pwa/src/templates/UnauthenticatedTemplate.tsx
+++ pwa/src/templates/UnauthenticatedTemplate.tsx
@@ -1,10 +1,15 @@
 import * as React from "react";
 import { Heading1 } from "@nl-design-system-unstable/example-next.js/src/components/utrecht";
+import { ProductGrid } from "../components/products/ProductGrid/ProductGrid";
+import APIService from "../apiService/apiService";
+import APIContext from "../apiService/apiContext";
 
 export const UnauthenticatedTemplate: React.FC = () => {
   return (
     <>
       <Heading1>Welcome to the Skeleton Application</Heading1>
+
+      <ProductGrid products={products} />
     </>
   );
 };
