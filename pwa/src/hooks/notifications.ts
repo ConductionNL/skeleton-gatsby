6fa28740263bd72dd64b29f3ec5b8c9c93ac2e15
Sarai Misidjan <Sarai@conduction.nl>
2022-04-13 10:25:37 +0200

    added notifications hook

---
+++ pwa/src/hooks/notifications.ts
@@ -0,0 +1,27 @@
+import * as React from "react";
+import { QueryClient, useMutation, useQuery } from "react-query";
+import APIService from "../apiService/apiService";
+import APIContext from "../apiService/apiContext";
+import { navigate } from "gatsby-link";
+import { addItem } from "../services/mutateQueries";
+
+export const useNotification = (queryClient: QueryClient) => {
+  const API = React.useContext(APIContext);
+  
+  const getAll = () =>
+    useQuery<any[], Error>("notifications", API.Notification.getAll, {
+      onError: (error) => {
+      },
+    });
+
+  const create = () =>
+    useMutation<any, Error, any>(API.Notification.create, {
+      onSuccess: async (newNotification) => {
+          addItem(queryClient, "notifications", newNotification);
+      },
+      onError: (error) => {
+      },
+    });
+
+  return { getAll, create };
+};
