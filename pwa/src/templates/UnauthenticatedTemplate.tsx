ea8d670b5364c14bba8d68956aec5d3a1c81c805
Barry Brands <barrybrands02@hotmail.com>
2022-04-13 14:46:00 +0200

    UAuthTemplate ProductGrid added

--- pwa/src/templates/UnauthenticatedTemplate.tsx
+++ pwa/src/templates/UnauthenticatedTemplate.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
 import { Heading1 } from "@nl-design-system-unstable/example-next.js/src/components/utrecht";
+import { ProductGrid } from "../components/products/ProductGrid/ProductGrid";
+import APIService from "../apiService/apiService";
+import APIContext from "../apiService/apiContext";
 
 export const UnauthenticatedTemplate: React.FC = () => {
   const [products, setProducts] = React.useState(null);
@@ -23,6 +26,7 @@ export const UnauthenticatedTemplate: React.FC = () => {
   return (
     <>
       <Heading1>Welcome to the Skeleton Application</Heading1>
+      <ProductGrid products={products} />
     </>
   );
 };
