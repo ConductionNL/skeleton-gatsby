9813e6641a62c1f1d0c442f8df150d0ce93c4cfb
Lennart <66552932+lencodes@users.noreply.github.com>
2022-04-13 09:38:29 +0200

    IMPLEMENT SideNav Utrecht Component wrapper

---
+++ pwa/src/components/utrecht/sideNav/SideNav.tsx
@@ -0,0 +1,67 @@
+/**
+ * This is a TEMPORARY wrapper over the Utrecht Breadcrumb component.
+ *
+ * Do NOT update this file, because all changes WILL BE LOST upon the package integration.
+ *
+ * Source: https://nl-design-system.github.io/utrecht/storybook/?path=/docs/css-component-navigatie-sidenav--side-nav
+ */
+
+import * as React from "react";
+import clsx from "clsx";
+
+interface ISideNavItem {
+  href: string;
+  title: string;
+  current?: boolean;
+  children?: {
+    href: string;
+    title: string;
+    current?: boolean;
+  }[];
+}
+
+interface SideNavProps {
+  items: ISideNavItem[];
+}
+
+export const SideNav: React.FC<SideNavProps> = ({ items }) => {
+  return (
+    <nav className="utrecht-sidenav">
+      <ul className="utrecht-sidenav__list">
+        {items.map((item) => (
+          <li className="utrecht-sidenav__item utrecht-sidenav__item--last utrecht-sidenav__item--parent">
+            <a
+              className="utrecht-sidenav__link utrecht-sidenav__link--current utrecht-sidenav__link--parent"
+              href={item.href}
+            >
+              <div
+                className={clsx("utrecht-sidenav__marker", item.current && "utrecht-sidenav__marker--current")}
+              ></div>
+              <div className="utrecht-sidenav__connection utrecht-sidenav__connection--parent utrecht-sidenav__connection--last"></div>
+              {item.title}
+            </a>
+
+            {item.children && (
+              <ul className="utrecht-sidenav__list utrecht-sidenav__list--child">
+                {item.children.map((child) => (
+                  <li className="utrecht-sidenav__item utrecht-sidenav__item--child">
+                    <a className="utrecht-sidenav__link utrecht-sidenav__link--child" href={child.href}>
+                      <div
+                        className={clsx(
+                          "utrecht-sidenav__marker utrecht-sidenav__marker--child",
+                          child.current && "utrecht-sidenav__marker--current",
+                        )}
+                      ></div>{" "}
+                      {child.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            )}
+            <div className="utrecht-sidenav__item-separator"></div>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
