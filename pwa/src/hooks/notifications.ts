96a8bd0cb151bcf5e342feb0299171d18c419fab
Lennart <66552932+lencodes@users.noreply.github.com>
2022-04-14 16:00:02 +0200

    MERGE CONFLICTS

--- pwa/src/hooks/notifications.ts
+++ pwa/src/hooks/notifications.ts
@@ -1,22 +1,22 @@
 import * as React from "react";
 import { QueryClient, useMutation, useQuery } from "react-query";
 import APIService from "../apiService/apiService";
-import { PrivateApiContext } from "../context/privateApi";
 import { navigate } from "gatsby-link";
 import { addItem } from "../services/mutateQueries";
+import APIContext from "../apiService/apiContext";
 
 export const useNotification = (queryClient: QueryClient) => {
-  const privateApi: APIService = React.useContext(PrivateApiContext);
+  const API: APIService = React.useContext(APIContext);
 
   const getAll = () =>
-    useQuery<any[], Error>("notifications", privateApi.Notification.getAll, {
+    useQuery<any[], Error>("notifications", API.Notification.getAll, {
       onError: (error) => {
         throw new Error(error.message);
       },
     });
 
   const create = () =>
-    useMutation<any, Error, any>(privateApi.Notification.create, {
+    useMutation<any, Error, any>(API.Notification.create, {
       onSuccess: async (newNotification) => {
         addItem(queryClient, "notifications", newNotification);
         navigate("/meldingen/overzicht");
