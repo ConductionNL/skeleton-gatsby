293674480e79d8b18305ad53a90656199e73b05c
Remko <remko@conduction.nl>
2022-04-15 14:00:25 +0200

    added kiss layout file

---
+++ pwa/src/layout/LayoutKiss.tsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+import "../styling/index.css";
+import "./../translations/i18n";
+import { Document, Page, PageContent } from "@nl-design-system-unstable/example-next.js/src/components/utrecht";
+import { isLoggedIn } from "../services/auth";
+import { APIProvider } from "../apiService/apiContext";
+import APIService from "../apiService/apiService";
+import Footer from "./../components/footer/Footer";
+import { HeaderTemplate } from "../templates/header/HeaderTemplate";
+import { GatsbyProvider, IGatsbyContext } from "../context/gatsby";
+import { useTranslation } from "react-i18next";
+
+interface LayoutKissProps {
+  children: React.ReactNode;
+  pageContext: any; // Gatsby pageContext
+  location: any; // Gatsby location
+}
+
+const LayoutKiss: React.FC<LayoutKissProps> = ({ children, pageContext, location }) => {
+  const { t } = useTranslation();
+  const [API, setAPI] = React.useState<APIService | null>(null);
+  const [gatsbyContext, setGatsbyContext] = React.useState<IGatsbyContext>({ ...{ pageContext, location } });
+
+  React.useEffect(() => {
+    setGatsbyContext({ ...{ pageContext, location } });
+  }, [pageContext, location]);
+
+  React.useEffect(() => {
+    if (!isLoggedIn()) {
+      setAPI(null);
+      return;
+    }
+
+    const jwt = sessionStorage.getItem("jwt");
+    !API && jwt && setAPI(new APIService(jwt));
+  }, [API, isLoggedIn()]);
+
+  return (
+    <GatsbyProvider value={gatsbyContext}>
+      <Document>
+        <Page className="Page">
+          <HeaderTemplate />
+          <PageContent className="PageContent">
+            <APIProvider value={API}>
+              <title>{t("Skeleton Application")}</title>
+              {children}
+            </APIProvider>
+          </PageContent>
+          <Footer />
+        </Page>
+      </Document>
+    </GatsbyProvider>
+  );
+};
+
+export default LayoutKiss;
