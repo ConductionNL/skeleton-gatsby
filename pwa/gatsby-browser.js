4830ad2f55cdc28476877ab3ff553c410b146d37
Barry Brands <barrybrands02@hotmail.com>
2022-04-22 12:52:05 +0200

    theme-switcher gatsby-browser

--- pwa/gatsby-browser.js
+++ pwa/gatsby-browser.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+import { UrlContextWrapper } from "./src/context/urlContext";
 
 export const wrapRootElement = ({ element }) => {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      {element}
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <UrlContextWrapper>
+      <QueryClientProvider client={queryClient}>
+        {element}
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </UrlContextWrapper>
   );
 };
