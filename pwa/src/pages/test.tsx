26b5b8802cfa2160677215260f8f9b6ba3214a33
Mark West <markwesterweel@conduction.nl>
2022-04-26 20:58:18 +0200

    documentation till api

--- pwa/src/pages/test.tsx
+++ pwa/src/pages/test.tsx
@@ -1,34 +1,21 @@
 import React, { useEffect, useState } from "react";
 import APIService from "../apiService/apiService";
 import APIContext from "../apiService/apiContext";
-import { ProductGrid } from "../components/products/ProductGrid/ProductGrid";
 
 const ExamplePage = (props: any) => {
-  const [products, setProducts] = useState<any>(null);
+  const [notifications, setNotifications] = useState<any>(null);
   const API: APIService = React.useContext(APIContext);
-  // function setAlert(arg0: { message: any; type: string }) {
-  //   throw new Error("Function not implemented.");
-  // }
 
   useEffect(() => {
-    handleSetProducts();
-  });
-
-  const handleSetProducts = (): void => {
-    API.Product.getAll().then((res) => {
-      console.log(res.data);
-
-      setProducts(res.data.content);
+    API.Notification.getAll().then((res) => {
+      setNotifications(res);
     });
-    // .catch((err) => {
-    //   setAlert({ message: err, type: "danger" });
-    //   throw new Error("GET products error: " + err);
-    // });
-  };
+  }, []);
+
   return (
     <>
       <h1>Example </h1>
-      <ProductGrid products={products} />
+      <div>Notifications{notifications?.length}</div>
     </>
   );
 };
